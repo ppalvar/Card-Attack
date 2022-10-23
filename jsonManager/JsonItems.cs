3a0719da1492f9fb100871996908e2d52a517cea
Pedro P. Alvarez Portelles <ppalvarez2002@gmail.com>
2022-10-23 15:46:15 -0400

    Updated JsonItems

--- jsonManager/JsonItems.cs
+++ jsonManager/JsonItems.cs
@@ -5,15 +5,17 @@ using System;
 /// <summary>
 /// Elemento de Json base para almacenar cartas en disco
 /// </summary>
-public abstract class CardItem{
+public abstract class CardJsonItem{
     public string name{ get; set; }
     public string description{ get; set; }
     public float appearingProbability{get;set;}
+    public string element{get;set;}
         
-    public CardItem(string name, string description, float appearingProbability){
+    public CardJsonItem(string name, string description, float appearingProbability, string element){
         this.name = name;
         this.description = description;
         this.appearingProbability = appearingProbability;
+        this.element = element;
     }
 
     public override string ToString()
@@ -25,15 +27,16 @@ public abstract class CardItem{
 /// <summary>
 /// Elemento especializado para guardar una carta de monstruo
 /// </summary>
-public class MonsterCardItem : CardItem{
+public class MonsterCardJsonItem : CardJsonItem{
     public int attack{ get; set; }
     public int hp{ get; set; }
 
-    public MonsterCardItem(string name,
+    public MonsterCardJsonItem(string name,
                            string description, 
                            float appearingProbability,
+                           string element,
                            int attack,
-                           int hp) : base(name, description, appearingProbability)
+                           int hp) : base(name, description, appearingProbability, element)
     {
         this.attack = attack;
         this.hp = hp;
@@ -44,13 +47,14 @@ public class MonsterCardItem : CardItem{
 /// <summary>
 /// Elemento especializado para guardar una carta de efecto
 /// </summary>
-public class EffectCardItem : CardItem{
+public class EffectCardJsonItem : CardJsonItem{
     public int attack{ get; set; }
     public int hp{ get; set; }
 
-    public EffectCardItem(string name,
+    public EffectCardJsonItem(string name,
                            string description, 
-                           float appearingProbability) : base(name, description, appearingProbability)
+                           float appearingProbability,
+                           string element) : base(name, description, appearingProbability, element)
     {
         //TODO
     }
