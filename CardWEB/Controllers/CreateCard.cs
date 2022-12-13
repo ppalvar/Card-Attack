efd586b57ab68350801aaad9718796ee5bac236a
Pedro P. Alvarez Portelles <ppalvarez2002@gmail.com>
2022-12-12 21:57:58 -0500

    refactored and commented

--- CardWEB/Controllers/CreateCard.cs
+++ CardWEB/Controllers/CreateCard.cs
@@ -2,46 +2,70 @@ namespace Controllers;
 
 using Cards;
 using CardFactorys;
-using JsonItems;
+using CardJsonItems;
 using System.Text;
 using System.Text.Json;
 
-public static class CardCreator {
-    public static void CreateMonsterCard(HttpContext request) {
+/// <summary>
+/// Static class with methods for managing new cards creation
+/// </summary>
+public static class CardCreator
+{
+    /// <summary>
+    /// Creates a new instance of MonsterCard and saves it to the disk
+    /// </summary>
+    /// <param name="request">An HttpContext with all the needed data in the body</param>
+    public static void CreateMonsterCard(HttpContext request)
+    {
         string json = GetJSON(request.Request.Body);
         MonsterCardJsonItem? monsterJSON = JsonSerializer.Deserialize<MonsterCardJsonItem>(json);
-        
-        if (monsterJSON != null) {
+
+        if (monsterJSON != null)
+        {
             MonsterCard monster = new MonsterCard(monsterJSON);
             CardFactory factory = new CardFactory();
             factory.CreateCard<MonsterCard>(monster);
         }
     }
 
-    public static void CreateEffectCard(HttpContext request) {
+    /// <summary>
+    /// Creates a new instance of EffectCard and saves it to the disk
+    /// </summary>
+    /// <param name="request">An HttpContext with all the needed data in the body</param>
+    public static void CreateEffectCard(HttpContext request)
+    {
         string json = GetJSON(request.Request.Body);
         EffectCardJsonItem? effectJSON = JsonSerializer.Deserialize<EffectCardJsonItem>(json);
 
-        if (effectJSON != null) {
+        if (effectJSON != null)
+        {
             EffectCard monster = new EffectCard(effectJSON);
             CardFactory factory = new CardFactory();
             factory.CreateCard<EffectCard>(monster);
         }
     }
 
-    public static string GetJSON(Stream s) {
-        byte[] buffer = new byte[1<<11];
+    /// <summary>
+    /// Reads a json from a Stream and casts it to a string
+    /// </summary>
+    /// <param name="s">A stream with the json</param>
+    /// <returns>The json as a string</returns>
+    public static string GetJSON(Stream s)
+    {
+        byte[] buffer = new byte[1 << 11];
 
-        s.ReadAsync(buffer,0,buffer.Length);
-        
+        s.ReadAsync(buffer, 0, buffer.Length);
+
         string json = Encoding.UTF8.GetString(buffer, 0, buffer.Length);
 
         int i = -1;
 
-        foreach (char? c in json) {
+        foreach (char? c in json)
+        {
             i++;
-            if (c == 0) {
-                
+            if (c == 0)
+            {
+
                 break;
             }
         }
--- CardWEB/Controllers/Game.cs
+++ CardWEB/Controllers/Game.cs
@@ -6,14 +6,27 @@ using Cards;
 using Models;
 using System.Text.Json;
 
-public static class Game {
-    private static Match? match{get;set;}
+/// <summary>
+/// Server class to manage all the actions in a match
+/// </summary>
+public static class Game
+{
+    private static Match? match { get; set; }
 
-    public static string NewGame(string type, HttpContext request) {
-        if (type == "human") {
-            match = new Match(MatchTypes.HumanVSHuman, 5, 5);
+    /// <summary>
+    /// Starts a new game
+    /// </summary>
+    /// <param name="type">"human" for a match against another person, "ai" for a match against the computer</param>
+    /// <param name="request">An HttpContext with all the needed data in the body</param>
+    /// <returns>A json string with the starting game state</returns>
+    public static string NewGame(string type, HttpContext request)
+    {
+        if (type == "human")
+        {
+            match = new Match(MatchTypes.HumanVSHuman, 5, 5, 10);
         }
-        else if (type == "ai"){
+        else if (type == "ai")
+        {
             match = new Match(MatchTypes.ComputerVSHuman, 5, 5);
         }
 
@@ -21,8 +34,10 @@ public static class Game {
 
         CardNames? names = JsonSerializer.Deserialize<CardNames>(json);
 
-        if (match != null) {
-            if (names != null) {
+        if (match != null)
+        {
+            if (names != null)
+            {
                 match.SetDeck(names.playerA, true);
                 match.SetDeck(names.playerB, false);
             }
@@ -32,11 +47,13 @@ public static class Game {
 
             match.BeginGame();
 
-            List <CardResponse> responseBody = new List<CardResponse>();
+            List<CardResponse> responseBody = new List<CardResponse>();
 
             int x = 0;
-            foreach (Cards.Card? c in match.player.Hand) {
-                if (c != null) {
+            foreach (Cards.Card? c in match.player.Hand)
+            {
+                if (c != null)
+                {
                     CardResponse r = new CardResponse(c, x);
                     responseBody.Add(r);
                 }
@@ -44,23 +61,31 @@ public static class Game {
             }
 
             string jsonResponse = JsonSerializer.Serialize(responseBody);
-            
+
             return jsonResponse;
         }
 
         throw new Exception("something happenned");
     }
 
-    public static string NewTurn(bool auto, HttpContext request) {
+    /// <summary>
+    /// Ends the current turn and begins another
+    /// </summary>
+    /// <param name="request">An HttpContext with all the needed data in the body</param>
+    /// <returns>A json string with the new match state</returns>
+    public static string NewTurn(bool auto, HttpContext request)
+    {
         if (match == null) throw new Exception("there is no match yet");
-        
-        if (!auto)match.EndTurn();
 
-        List <CardResponse> responseBody = new List <CardResponse>();
+        if (!auto) match.EndTurn();
+
+        List<CardResponse> responseBody = new List<CardResponse>();
 
         int i = 0;
-        foreach (Cards.Card? c in match.player.Hand) {
-            if (c != null) {
+        foreach (Cards.Card? c in match.player.Hand)
+        {
+            if (c != null)
+            {
                 CardResponse r = new CardResponse(c, i);
                 responseBody.Add(r);
             }
@@ -68,21 +93,29 @@ public static class Game {
         }
 
         string jsonResponse = JsonSerializer.Serialize(responseBody);
-        
+
         return jsonResponse;
     }
 
-    public static string Play(HttpContext request) {
+    /// <summary>
+    /// Puts a card in the table or adds a power to a monster
+    /// </summary>
+    /// <param name="request">An HttpContext with all the needed data in the body</param>
+    /// <returns>A json string with the velidness of the movement</returns>
+    public static string Play(HttpContext request)
+    {
         if (match == null) throw new Exception("there is no active match yet");
 
         string _json = CardCreator.GetJSON(request.Request.Body);
         InvokeRequest? action = JsonSerializer.Deserialize<InvokeRequest>(_json);
 
         ActionResponse response = new ActionResponse(false, false, false);
-        if (action != null && match.player.Hand[action.CardIndex] is MonsterCard monster) { 
-            if (monster != null) {
+        if (action != null && match.player.Hand[action.CardIndex] is MonsterCard monster)
+        {
+            if (monster != null)
+            {
                 int turnCount = match.TurnCounter;
-                match.Play(action.CardIndex, null);
+                match.Play(action.CardIndex);
 
                 response.canMove = true;
             }
@@ -91,36 +124,51 @@ public static class Game {
         return JsonSerializer.Serialize(response);
     }
 
-    public static string Attack(int card, int target, HttpContext request) {
+    /// <summary>
+    /// Attacks a monster in the enemy's table
+    /// </summary>
+    /// <param name="card">The index in the table of the card will attack</param>
+    /// <param name="target">The index in the enemy's table where the attack will be made</param>
+    /// <param name="request">An HttpContext with all the needed data in the body</param>
+    /// <returns>A json string with the new state of the match</returns>
+    public static string Attack(int card, int target, HttpContext request)
+    {
         if (match == null) throw new Exception("there is no active match yet");
 
         MonsterCard? _target = match.enemy.Table[target];
         ActionResponse response = new ActionResponse(false, false, false);
 
-        if (_target != null && match.player.Table[card] != null) {
-            try {
+        if (_target != null && match.player.Table[card] != null)
+        {
+            try
+            {
                 response = new ActionResponse(true, match.Attack(card, _target), match.Winner() != null);
 
                 response.Hand = new CardResponse[match.player.Hand.Length];
-                for (int i = 0; i < match.player.Hand.Length; i++) {
+                for (int i = 0; i < match.player.Hand.Length; i++)
+                {
                     Card? m = match.player.Hand[i];
 
-                    if (m != null) {
+                    if (m != null)
+                    {
                         response.Hand[i] = new CardResponse(m, i);
                     }
                 }
 
                 response.TableA = new CardResponse[match.player.Table.Length];
                 response.TableB = new CardResponse[match.player.Table.Length];
-                
-                for (int i = 0; i < match.player.Table.Length; i++) {
-                    MonsterCard? p = (MonsterCard?) match.player.Table[i];
-                    MonsterCard? e = (MonsterCard?) match.enemy.Table[i];
 
-                    if (p != null) {
+                for (int i = 0; i < match.player.Table.Length; i++)
+                {
+                    MonsterCard? p = (MonsterCard?)match.player.Table[i];
+                    MonsterCard? e = (MonsterCard?)match.enemy.Table[i];
+
+                    if (p != null)
+                    {
                         response.TableA[i] = new CardResponse(p, i);
                     }
-                    if (e != null) {
+                    if (e != null)
+                    {
                         response.TableB[i] = new CardResponse(e, i);
                     }
                 }
@@ -129,7 +177,8 @@ public static class Game {
 
                 return JsonSerializer.Serialize(response);
             }
-            catch (Exception e){
+            catch (Exception e)
+            {
                 Console.WriteLine(e.Message);
             }
         }
@@ -137,7 +186,14 @@ public static class Game {
         return JsonSerializer.Serialize(response);
     }
 
-    public static string DropCard(int index, HttpContext httpContext) {
+    /// <summary>
+    /// Drops a given card from the hand
+    /// </summary>
+    /// <param name="index">The index in the hand of the card that will be droppeds</param>
+    /// <param name="request">An HttpContext with all the needed data in the body</param>
+    /// <returns>A json string containig de validity of this action</returns>
+    public static string DropCard(int index, HttpContext request)
+    {
         if (match == null) throw new Exception("there is no active match yet");
 
         bool flag = match.player.DropCard(index);
@@ -146,16 +202,26 @@ public static class Game {
         return JsonSerializer.Serialize(new ActionResponse(flag, false, endGame));
     }
 
-    public static string EquipPower(int cardIndex, int targetIndex, HttpContext context) {
+    /// <summary>
+    /// Equips a power to a Monster
+    /// </summary>
+    /// <param name="cardIndex">The card in the hand</param>
+    /// <param name="targetIndex">The card in the table</param>
+    /// <param name="request">An HttpContext with all the needed data in the body</param>
+    /// <returns>A json string containing the validity of the action</returns>
+    public static string EquipPower(int cardIndex, int targetIndex, HttpContext request)
+    {
         if (match == null) throw new Exception("there is no active match yet");
 
         bool flag = false;
 
-        try {
+        try
+        {
             match.Play(cardIndex, match.player.Table[targetIndex]);
             flag = true;
         }
-        catch {
+        catch
+        {
             // do nothing
         }
 
@@ -164,33 +230,50 @@ public static class Game {
         return JsonSerializer.Serialize(new ActionResponse(flag, false, endGame));
     }
 
-    public static string UsePower(int cardindex, int targetIndex, string powerName, HttpContext context) {
+    /// <summary>
+    /// Uses a power
+    /// </summary>
+    /// <param name="cardindex">The card that will use a power</param>
+    /// <param name="targetIndex">The victim of this attack</param>
+    /// <param name="powerName">The full name of the power that will be used</param>
+    /// <param name="request">An HttpContext with all the needed data in the body</param>
+    /// <returns>A json string containing the new state of the match</returns>
+    public static string UsePower(int cardindex, int targetIndex, string powerName, HttpContext request)
+    {
         if (match == null) throw new Exception("there is no active match yet");
 
         ActionResponse response = new ActionResponse(false, false, false);
 
-        if (cardindex <= match.player.Table.Length && targetIndex <= match.enemy.Table.Length) {
-            MonsterCard? monster = (MonsterCard?) match.player.Table[cardindex];
+        if (cardindex <= match.player.Table.Length && targetIndex <= match.enemy.Table.Length)
+        {
+            MonsterCard? monster = (MonsterCard?)match.player.Table[cardindex];
 
-            if (monster != null) {
+            if (monster != null)
+            {
                 int power = -1;
-                for (int i = 0; i < monster.Powers.Length; i++) {
+                for (int i = 0; i < monster.Powers.Length; i++)
+                {
                     Power? p = monster.Powers[i];
 
-                    if (p != null && p.Name == powerName) {
+                    if (p != null && p.Name == powerName)
+                    {
                         power = i;
                     }
                 }
 
-                if (power != -1) {
-                    MonsterCard? target = (MonsterCard?) match.enemy.Table[targetIndex];
-                    if (target != null) {
-                        try {
+                if (power != -1)
+                {
+                    MonsterCard? target = (MonsterCard?)match.enemy.Table[targetIndex];
+                    if (target != null)
+                    {
+                        try
+                        {
                             response.turnEnds = match.UsePower(cardindex, power, target);
 
                             response.canMove = true;
                         }
-                        catch (Exception e) {
+                        catch (Exception e)
+                        {
                             Console.WriteLine(e.Message);
                         }
                     }
@@ -201,25 +284,30 @@ public static class Game {
         response.gameEnds = match.Winner() != null;
 
         response.Hand = new CardResponse[match.player.Hand.Length];
-        for (int i = 0; i < match.player.Hand.Length; i++) {
+        for (int i = 0; i < match.player.Hand.Length; i++)
+        {
             Card? m = match.player.Hand[i];
 
-            if (m != null) {
+            if (m != null)
+            {
                 response.Hand[i] = new CardResponse(m, i);
             }
         }
 
         response.TableA = new CardResponse[match.player.Table.Length];
         response.TableB = new CardResponse[match.player.Table.Length];
-        
-        for (int i = 0; i < match.player.Table.Length; i++) {
-            MonsterCard? p = (MonsterCard?) match.player.Table[i];
-            MonsterCard? e = (MonsterCard?) match.enemy.Table[i];
 
-            if (p != null) {
+        for (int i = 0; i < match.player.Table.Length; i++)
+        {
+            MonsterCard? p = (MonsterCard?)match.player.Table[i];
+            MonsterCard? e = (MonsterCard?)match.enemy.Table[i];
+
+            if (p != null)
+            {
                 response.TableA[i] = new CardResponse(p, i);
             }
-            if (e != null) {
+            if (e != null)
+            {
                 response.TableB[i] = new CardResponse(e, i);
             }
         }
