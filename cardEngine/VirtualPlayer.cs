395f796739151010a2106b9d59866c30b347c338
Pedro P. Alvarez Portelles <ppalvarez2002@gmail.com>
2022-12-13 23:35:52 -0500

    added alpha virtual player

--- cardEngine/VirtualPlayer.cs
+++ cardEngine/VirtualPlayer.cs
@@ -1,33 +1,118 @@
 namespace Players;
 
 using Match;
+using Cards;
 
 public class VirtualPlayer : Player{
     public VirtualPlayer(int cardsInHand, int cardsInTable) : base(cardsInHand, cardsInTable){
-        //do nothing
+        this.cardsInHand = cardsInHand;
+        this.cardsInTable = cardsInTable;
     }
+
+    int cardsInHand;
+    int cardsInTable;
+    float maxAtkEvaluation = float.MaxValue;
+    Match[] movement = new Match[6];
+
 
     public override void BeginTurn(Match? match) {
         if (match != null) {
             base.BeginTurn(match);
 
-            MakeMovement(match.Clone());
+            MakeMovement(match);
 
-            EndTurn();
+            match.EndTurn();
         }
     }
 
     public void MakeMovement(Match match){
-        #region Stage #1: Invoke Monsters
-            //todo
-        #endregion
+        movement[0] = PlayMonster(match);
 
-        #region Stage #2: Add powers to monsters
-            //todo
-        #endregion
+        // for(int i = 0; i < cardsInTable; i++){
+        //     movement[i+1] = AddPowers(match, i);
+        // }
+    }
 
-        #region Stage #3: Attack
-            //todo
-        #endregion
+    Match PlayMonster(Match match){
+        int max = 0;
+        int card = 0;
+        
+        if(match.player.Table.Contains(null))
+            for(int i = 0; i < cardsInHand; i++)
+                if(match.player.Hand[i] is MonsterCard m && m.HP > max){
+                    max = m.HP;
+                    card = i;
+                }
+        // Console.WriteLine(match.player.Hand[card]);
+        match.Play(card);
+
+        return match;
+    }
+
+    Match AddPowers(Match match, int i){
+        MonsterCard? m = match.player.Table[i];
+        Match best = match.Clone();
+        float max = float.MaxValue;
+
+        if(m != null && m.Powers.Contains(null)){
+            for(int j = 0; j < cardsInHand; j++)
+                if(match.player.Hand[j] is EffectCard e && !m.Powers.Contains(e.power)){
+                    Match clone = match.Clone();
+                        
+                    clone.Play(j, m);
+
+                    clone = AtkCard(clone, i, j);    
+
+                    if(max > maxAtkEvaluation){
+                        best = clone;
+                        max = maxAtkEvaluation;
+                    }  
+                }      
+        } 
+
+        return best;    
+    }
+
+    Match AtkCard(Match match, int card, int power){
+        MonsterCard? m = match.player.Table[card];
+        Match bestOpcion = match.Clone();
+
+        if(m != null){
+            for(int i = 0; i < cardsInTable; i++)
+                if(match.enemy.Table[i] is MonsterCard enemy && enemy != null){
+                    Match clone = match.Clone();
+                    float n = 0;
+
+                    if(power == m.Powers.Length){
+                        clone.Attack(card, enemy);
+
+                        n = Evaluate(clone);
+                    }
+
+                    else if(m.Powers[power] != null){
+                        clone.UsePower(card,power,enemy);
+
+                        n = Evaluate(clone);
+                    }
+
+                    if(n < maxAtkEvaluation){
+                        maxAtkEvaluation = n;
+                        bestOpcion = clone;
+                    }
+                }
+        }
+
+        return bestOpcion;
+    }
+
+    float Evaluate(Match match){
+        float evaluation = 0;
+
+        foreach(MonsterCard? i in match.enemy.Table)
+            if(i != null){
+                evaluation += 10 + i.HP / 4;
+            }
+
+        return evaluation;
     }
 }
