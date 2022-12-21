2e07e7ab10bb35029e71a8c214e0128433eedf2b
Pedro P. Alvarez Portelles <ppalvarez2002@gmail.com>
2022-12-21 16:47:22 -0500

    added try parse feature

--- interpreterMLC/Interpreter.cs
+++ interpreterMLC/Interpreter.cs
@@ -4,22 +4,18 @@ using Parser;
 
 public class Interpreter
 {
-    private Parser? Parser { get; set; }
-
-    public Interpreter() { }
-
     /// <summary>
     /// Interprets a given string input as a code script
     /// </summary>
     /// <param name="Text">The code itself</param>
     /// <param name="Context">A C# object that can be modified from the code</param>
-    public void Interpret(string Text, object Context)
+    public static void Interpret(string Text, object Context)
     {
         Symbols.SymbolTable.InitBuiltins();
         Symbols.Scope.Clear();
 
-        this.Parser = new Parser(Text);
-        AST? Tree = Parser.Parse();
+        Parser parser = new Parser(Text);
+        AST? Tree = parser.Parse();
         try
         {
             NodeVisitor.Visit(Tree, Context, true);
@@ -28,5 +24,20 @@ public class Interpreter
         {
             throw new Exception(e.Message);
         }
+    }
+
+    public static Exception? TryParse(string Text) {
+        Symbols.SymbolTable.InitBuiltins();
+        Symbols.Scope.Clear();
+
+        Parser parser = new Parser(Text);
+        try {
+            AST? Tree = parser.Parse();
+        }
+        catch (Exception e) {
+            return e;
+        }
+
+        return null;
     }
 }
